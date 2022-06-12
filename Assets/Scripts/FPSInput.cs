78c8dabcb9b031ab08cae230dfd1027ac61d898b
deft <deft1991@gmail.com>
2022-06-11 20:50:53 -0700

    Fix

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Photon.Pun;
+// using Photon.Pun;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
